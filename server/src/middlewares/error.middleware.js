9f5c54ab05a723640d0370f4dc6ee8e1c0b0ee7b
kovaltar <kovaltar@gmail.com>
2025-05-19 02:30:17 +0200

    Solution fixes-2

--- server/src/middlewares/error.middleware.js
+++ server/src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ import { ApiError } from "../exeptions/api.error.js";
 
 export const errorMiddleware = (error, req, res, next) => {
   if (error instanceof ApiError) {
-    res.status(error.status).send({
+    return res.status(error.status).send({
       message: error.message,
       errors: error.errors,
     });
